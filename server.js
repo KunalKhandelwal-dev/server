42e387545dbfb4f46bdf21470c6e46b1e001049a
kunal131005 <kunalkhandelwal155@gmail.com>
2025-11-20 14:00:27 +0530

    Back to same, as before of this commit.

--- server.js
+++ server.js
@@ -117,7 +117,7 @@ async function saveToSheet(data, fileUrl) {
 
     await sheets.spreadsheets.values.append({
       spreadsheetId: SPREADSHEET_ID,
-      range: "Submissions!B:P",
+      range: "Submissions!B:O",
       valueInputOption: "RAW",
       requestBody: {
         values: [
@@ -136,8 +136,6 @@ async function saveToSheet(data, fileUrl) {
             upiId || "-",
             transactionId || "-",
             new Date().toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
-            "-",
-            email || "-"
           ],
         ],
       },
